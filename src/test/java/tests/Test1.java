811c924d499979934389b209ac7cc8fc38219272
Marina Roginski <marinaroginski@gmail.com>
2024-02-26 18:21:08 +0300

    Initial commit

---
+++ src/test/java/tests/Test1.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.example.Main;
+import org.testng.annotations.Test;
+
+public class Test1 {
+    @Test
+    public void test1(){
+        System.out.println((Main.five()));
+    }
+}
